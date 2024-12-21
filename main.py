3467a193549e15cf50306b1e648b2265898957fd
NotTorne <135161351+NotTorne@users.noreply.github.com>
2024-12-21 20:17:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import game
+import numpy as np
+
+ 
+g = game.Game("dqn_agent", "cpu")
+
+
+hyperparameter = {
+  "lr_start": 1e-4,
+  "lr_end": 1e-4,
+  "batch_size": 128,
+  "gamma": 0.9,
+  "eps_start": 0.9,
+  "eps_end": 1e-2
+}
+    
+
+g.train_agent(False, 100, 100, hyperparameter)  
+score = g.main(True)
+print("Score: ", score)
